c4ff3ade27fec729c7b316e6a896ab2bc1c8ee02
Allen Benny <48691514+officiallygod@users.noreply.github.com>
2021-03-12 19:21:03 +0530

    V 2.0.0

--- js/script.js
+++ js/script.js
@@ -1,26 +1,74 @@
-$(document).ready(function () {
-  $(window).scroll(function () {
-    if (this.scrollY > 20) $(".navbar").addClass("sticky");
-    else $(".navbar").removeClass("sticky");
-  });
+$( document ).ready( function () {
+    $( window ).scroll( function () {
+        if ( this.scrollY > 20 ) 
+            $( ".navbar" ).addClass( "sticky" );
+         else 
+            $( ".navbar" ).removeClass( "sticky" );
+        
 
-  $(window).scroll(function () {
-    if (this.scrollY > 1200) $(".skill-card__skillbar--percentage--level-1").addClass("anim-skill-1");
-    else $(".skill-card__skillbar--percentage--level-1").removeClass("anim-skill-1");
-  });
 
-  $(window).scroll(function () {
-    if (this.scrollY > 1200) $(".skill-card__skillbar--percentage--level-2").addClass("anim-skill-2");
-    else $(".skill-card__skillbar--percentage--level-2").removeClass("anim-skill-2");
-  });
+    } );
 
-  $(window).scroll(function () {
-    if (this.scrollY > 1200) $(".skill-card__skillbar--percentage--level-3").addClass("anim-skill-3");
-    else $(".skill-card__skillbar--percentage--level-3").removeClass("anim-skill-3");
-  });
+    $( window ).scroll( function () {
+        if ( this.scrollY > 1200 ) 
+            $( ".skill-card__skillbar--percentage--level-1" ).addClass( "anim-skill-1" );
+         else 
+            $( ".skill-card__skillbar--percentage--level-1" ).removeClass( "anim-skill-1" );
+        
 
-  $(".navbar__inner--menu-toggler").click(function () {
-    $(this).toggleClass("active");
-    $(".navbar__inner--menu").toggleClass("active");
-  });
-});
+
+    } );
+
+    $( window ).scroll( function () {
+        if ( this.scrollY > 1200 ) 
+            $( ".skill-card__skillbar--percentage--level-2" ).addClass( "anim-skill-2" );
+         else 
+            $( ".skill-card__skillbar--percentage--level-2" ).removeClass( "anim-skill-2" );
+        
+
+
+    } );
+
+    $( window ).scroll( function () {
+        if ( this.scrollY > 1200 ) 
+            $( ".skill-card__skillbar--percentage--level-3" ).addClass( "anim-skill-3" );
+         else 
+            $( ".skill-card__skillbar--percentage--level-3" ).removeClass( "anim-skill-3" );
+        
+
+
+    } );
+
+    $( ".navbar__inner--menu-toggler" ).click( function () {
+        $( this ).toggleClass( "active" );
+        $( ".navbar__inner--menu" ).toggleClass( "active" );
+    } );
+} );
+
+function submitFormFunc() {
+    console.log( "HUHUH" );
+    var name = document.getElementById( "name" );
+    var email = document.getElementById( "email" );
+
+    const Url = "https://konnect-js.herokuapp.com/konnectsnd";
+
+    const dataObject = {
+        "uid": "DfDMxNhG9nbgvclOh4YxjQ9NX9H2",
+        "name": name.value,
+        "email": email.value
+    };
+
+    var data = fetch( Url, {
+        method: "POST",
+        form: dataObject
+    } ).then( response => {
+
+        console.log( "IT RANNN" );
+        console.log( response.text() );
+
+    } ).then( html => {
+        console.log( html )
+    } );
+
+    console.log( data );
+}
